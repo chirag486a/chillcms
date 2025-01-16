27cdf5c4d9ae2bbd4feb3e26f619225aeac87908
chirag <70309029+chirag486a@users.noreply.github.com>
2025-01-16 23:50:50 +0545

    Edit Styles Decrease Button Size and also decrease status column width

--- src/components/ContentManagementTC.jsx
+++ src/components/ContentManagementTC.jsx
@@ -4,20 +4,18 @@ import { format } from "timeago.js";
 
 export default function ContentManagementTC({ content }) {
   return (
-    <button className="btn btn-ghost pl-0 inline-block h-fit w-fit rounded-lg font-thin">
-      <div className="flex flex-row w-fit rounded-sm pl-0  py-4 items-center gap-2">
+    <button className="btn btn-ghost border-base-300 border-0 border-b first:border-t pl-0 pr-4  rounded-none  inline-block h-fit w-fit  font-thin">
+      <div className="flex flex-row w-fit rounded-lg pl-0  py-0 items-center gap-2">
         <div className="w-fit">
           <Checkbox />
         </div>
-        <div
-          className="text-left max-w-96 min-w-96 overflow-ellipsis overflow-hidden"
-        >
+        <div className="text-left max-w-96 min-w-96 overflow-ellipsis overflow-hidden">
           <span className="text-nowrap max-w-96 overflow-ellipsis">
             {content.title}
           </span>
         </div>
         <div className="w-40 text-center">{format(content.createdAt)}</div>
-        <div className="w-40 text-center">{content.status}</div>
+        <div className="w-20 text-right">{content.status}</div>
       </div>
     </button>
   );
