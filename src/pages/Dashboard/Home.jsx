1712769fa1555a91cb8486da5ba87bea1438acbe
chirag <70309029+chirag486a@users.noreply.github.com>
2025-01-17 12:08:27 +0545

    Fix Responsiveness

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -51,44 +51,51 @@ export default function Home() {
   ];
 
   return (
-    <div className="prose max-w-none h-full w-full px-12 py-8 flex flex-col">
-      <h2>Dashboard</h2>
-      <div className="flex w-full gap-24 md:gap-48 xl:gap-96">
-        <div className="flex flex-col items-center justify-crnter tooltip tooltip-right" data-tip="124938">
-          <UsersIcon className="h-32 w-32 fill-base-content/80 mb-2" />
-          <small className="block">No. of users</small>
-          <span className="block text-4xl font-thin mt-3">124K</span>
-        </div>
-        <div className="flex flex-col items-center justify-center w-fit tooltip tooltip-right" data-tip="97102 MB">
-          <DiskOccupiedIcon className="h-32 w-32  fill-base-content/80 mb-2" />
-          <small className="block">Disk Usage</small>
-          <span className="block text-4xl font-thin mt-3">97GB</span>
-        </div>
-        <div className="flex flex-col items-center justify-center w-fit tooltip tooltip-right" data-tip="12345">
-          <OnlineUserIcon className="h-32 w-32 fill-base-content/80 mb-2" />
-          <small className="block">Active Users</small>
-          <span className="block text-4xl font-thin mt-3">12k</span>
-        </div>
-      </div>
-      <div className="flex gap-8">
-        <div className="w-1/3">
-          <h3>Recent Users</h3>
-          <div className="flex  flex-col">
-            <RecentItem details={users[1]} />
-            <RecentItem details={users[2]} />
-            <RecentItem details={users[3]} />
-            <RecentItem details={users[4]} />
-            <RecentItem details={users[0]} />
+    <div>
+      <div className="prose max-w-none h-full w-full px-12 py-8 flex flex-col">
+        <h2>Dashboard</h2>
+        <div className="flex w-full gap-24 md:gap-26 xl:gap-96 mb-12">
+          <div
+            className="flex flex-col items-center justify-crnter tooltip tooltip-right"
+            data-tip="124938"
+          >
+            <UsersIcon className="sm:h-24 sm:w-24 w-20 h-20 lg:h-32 lg:w-32 fill-base-content/80 mb-2" />
+            <small className="block">No. of users</small>
+            <span className="block text-4xl font-thin mt-3">124K</span>
+          </div>
+          <div
+            className="flex flex-col items-center justify-center w-fit tooltip tooltip-right"
+            data-tip="97102 MB"
+          >
+            <DiskOccupiedIcon className="sm:h-24 sm:w-24 w-20 h-20 lg:h-32 lg:w-32 fill-base-content/80 mb-2" />
+            <small className="block">Disk Usage</small>
+            <span className="block text-4xl font-thin mt-3">97GB</span>
+          </div>
+          <div
+            className="flex flex-col items-center justify-center w-fit tooltip tooltip-right"
+            data-tip="12345"
+          >
+            <OnlineUserIcon className="sm:h-24 sm:w-24 w-20 h-20 lg:h-32 lg:w-32 fill-base-content/80 mb-2" />
+            <small className="block">Active Users</small>
+            <span className="block text-4xl font-thin mt-3">12k</span>
           </div>
         </div>
-        <div className="w-1/3">
-          <h3>Recent Posts</h3>
-          <div className="flex  flex-col">
-            <RecentItem details={posts[0]} />
-            <RecentItem details={posts[1]} />
-            <RecentItem details={posts[2]} />
-            <RecentItem details={posts[3]} />
-            <RecentItem details={posts[4]} />
+        <div className="flex gap-8 w-full">
+          <div className="lg:w-1/3 w-1/2 md:w-1/2 xl:w-1/3">
+            <h3>Recent Users</h3>
+            <div className="flex  flex-col">
+              <RecentItem details={users[1]} />
+              <RecentItem details={users[2]} />
+              <RecentItem details={users[3]} />
+            </div>
+          </div>
+          <div className="lg:w-1/3 w-1/2 md:w-1/2 xl:w-1/3">
+            <h3>Recent Posts</h3>
+            <div className="flex  flex-col w-fit">
+              <RecentItem details={posts[0]} />
+              <RecentItem details={posts[1]} />
+              <RecentItem details={posts[2]} />
+            </div>
           </div>
         </div>
       </div>
