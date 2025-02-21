2d626e0cd62477342900345b5ddbd2cee7a28505
chirag <70309029+chirag486a@users.noreply.github.com>
2025-02-21 22:03:01 +0545

    Add MapControllers to Map Controllers to respective api routes

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -21,13 +21,9 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
