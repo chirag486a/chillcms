91d500d927c5aef03a86039c001634b8e80c43ce
chirag <70309029+chirag486a@users.noreply.github.com>
2025-02-22 18:21:51 +0545

    Fix Types and Migrations

--- Backend/Data/ApplicationDbContext.cs
+++ Backend/Data/ApplicationDbContext.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
         {
