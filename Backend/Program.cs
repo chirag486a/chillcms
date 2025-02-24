e4285ae497ddf5ff2e0920921ade6505e3405dc3
chirag <70309029+chirag486a@users.noreply.github.com>
2025-02-24 17:09:49 +0545

    Change AddAuthentication configuration options

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,15 +1,22 @@
+using System.Text;
 using Backend.Data;
+using Backend.Helpers;
+using Backend.Interfaces.IServices;
 using Backend.Models;
+using Backend.Services;
+using BCrypt.Net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddEndpointsApiExplorer();
 
 // Configure EF Core with SQL Server
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -20,10 +27,60 @@ builder.Services.AddIdentity<AppUser, IdentityRole>()
 
 builder.Services.AddControllers();
 
+
+builder.Services.AddScoped<IPasswordHasher<AppUser>, BCryptPasswordHasher>();
+
+// Load Jwt from appsettings.json
+var jwtSettings = builder.Configuration.GetSection("JWT");
+var keyString = jwtSettings["Key"];
+
+if (string.IsNullOrEmpty(keyString))
+{
+    throw new Exception("JWT Key is missing from configuration");
+}
+if (string.IsNullOrEmpty(jwtSettings["Issuer"]) || string.IsNullOrEmpty(jwtSettings["Audience"]))
+{
+    Console.WriteLine("Warning: Issuer or Audience is missing. Tokens might not validate correctly.");
+}
+
+var key = Encoding.ASCII.GetBytes(keyString);
+
+// Add authentication system
+builder.Services.AddAuthentication(options =>
+{
+    // options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    // options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+
 var app = builder.Build();
 
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
