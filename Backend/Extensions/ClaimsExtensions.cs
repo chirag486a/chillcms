98bc42af568e40f8c9f215d6a3f36ce8fae147a7
chirag <70309029+chirag486a@users.noreply.github.com>
2025-02-24 16:57:03 +0545

    Add Claim Extensions

---
+++ Backend/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Backend.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static string GetUserName(this ClaimsPrincipal user)
+        {
+            return user.Claims.SingleOrDefault(x => x.Type.Equals(ClaimTypes.GivenName)).Value;
+        }
+        public static string GetEmail(this ClaimsPrincipal user)
+        {
+            return user.Claims.SingleOrDefault(x => x.Type.Equals(ClaimTypes.Email)).Value;
+        }
+    }
+}
