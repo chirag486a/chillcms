19be862d7aaa4a2b5dd751a722a5263dc62d1ae6
chirag <70309029+chirag486a@users.noreply.github.com>
2025-02-24 17:07:27 +0545

    Add Create new password route and edit other routes

--- Backend/Controllers/AccountController.cs
+++ Backend/Controllers/AccountController.cs
@@ -4,61 +4,164 @@ using System.Linq;
 using System.Threading.Tasks;
 using Backend.Data;
 using Backend.Dtos.AppUser;
+using Backend.Extensions;
+using Backend.Interfaces.IServices;
 using Backend.Mappers;
 using Backend.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Backend.Controllers
 {
     [ApiController]
-    [Route("api/")]
+    [Route("api/accounts")]
     public class AccountController : ControllerBase
     {
         UserManager<AppUser> _userManager;
-        public AccountController(UserManager<AppUser> userManager)
+        ITokenService _tokenService;
+        SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
+            _signInManager = signInManager;
         }
 
         [HttpPost("signup")]
         public async Task<IActionResult> Signup([FromBody] SignupDto signup)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
-            }
-            var NewUser = signup.ToAppUserFromSignupDto();
 
-            // Done to remove editor warning...
-            if (NewUser.Email == null) return BadRequest("Email is invalid");
-            var ifExists = await _userManager.FindByEmailAsync(NewUser.Email);
-            if (ifExists != null)
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var NewUser = signup.ToAppUserFromSignupDto();
+
+                // Done to remove editor warning...
+                if (NewUser.Email == null) return BadRequest("Email is invalid");
+                var ifExists = await _userManager.FindByEmailAsync(NewUser.Email);
+                if (ifExists != null)
+                {
+                    return BadRequest("User Already Exist");
+                }
+
+                var result = await _userManager.CreateAsync(NewUser, signup.Password);
+
+                if (result.Succeeded)
+                {
+                    return Ok(NewUser);
+                }
+                return Ok("Something went wrong");
+            }
+            catch (Exception err)
             {
-                return BadRequest("User Already Exist");
+                Console.WriteLine(err);
+                return Ok("Something went wrong");
             }
-
-            var result = await _userManager.CreateAsync(NewUser, signup.Password);
-
-            if (result.Succeeded)
-            {
-                return Ok(NewUser);
-            }
-
-            return Ok("Something went wrong");
         }
 
         [HttpPost("login")]
-        public IActionResult Login([FromBody] LoginDto loginDto)
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+                AppUser? loginUser = null;
+                if (!string.IsNullOrWhiteSpace(loginDto.Email))
+                    loginUser = await _userManager.FindByEmailAsync(loginDto.Email);
+
+                if (loginUser == null)
+                {
+                    var errorResponse = new
+                    {
+                        message = "Could not find user"
+                    };
+                    return NotFound(errorResponse);
+                }
+
+                var results = await _signInManager.CheckPasswordSignInAsync(loginUser, loginDto.Password, false);
+
+                if (!results.Succeeded)
+                {
+                    var errorResponse = new
+                    {
+                        message = "Username not found or Incorrect Password"
+                    };
+
+                    return Unauthorized(errorResponse);
+                }
+
+                var response = new
+                {
+                    Name = loginUser.Name,
+                    Email = loginUser.Email,
+                    Token = _tokenService.GenerateToken(loginUser),
+                };
+                return Ok(response);
             }
-            // Load user manager
-            // Search If user already exists
-            // Create user (Repository Pattern)
-            return Ok("hello");
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+                var errResponse = new
+                {
+                    message = "Something went wrong"
+                };
+                return BadRequest(errResponse);
+            }
         }
+
+        [Authorize]
+        [HttpPatch("changePassword")]
+        public async Task<IActionResult> ChangePassword([FromBody] PasswordDto passwordDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var email = User.GetEmail();
+
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    var errorResponse = new
+                    {
+                        message = "User not logged in."
+                    };
+                    return Unauthorized(errorResponse);
+                }
+
+
+                var result = await _userManager.ChangePasswordAsync(user, passwordDto.OldPassword, passwordDto.OldPassword);
+
+                if (!result.Succeeded)
+                {
+                    return Ok(result.Errors);
+                }
+
+                var response = new
+                {
+                    status = "success",
+                    message = "Change Passworrd successfully",
+                };
+                return Ok(response);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+                return BadRequest(new
+                {
+                    message = "Something went wrong"
+                });
+            }
+        }
+
     }
 }
